af748ea9e0158fac9b1820fecf6783420d67a8f9
Christian Zimpelmann <zimpelmann.christian@gmail.com>
2021-01-31 08:25:56 +0100

    started with presentation for panel discussion.

---
+++ panel_discussion_law.md
@@ -0,0 +1,78 @@
+---
+title: Home Office während der CoVid-19 Pandemie??
+author: Christian Zimpelmann
+organization: IZA - Institute of Labor Economics
+---
+
+
+CoViD-19 Impact Lab
+===================
+
+* Gruppe von Wissenschaftler:innen des IZA und der Universitäten Bonn, Mannheim, Tilburg 
+* Impuls: Pandemie wirtschafts- und sozialwissenschaftlich begleiten
+
+
+LISS Panel
+==========
+
+* Panel: Dieselben Personen / Haushalte werden immer wieder befragt
+* Online, läuft seit 2007
+* Wahrscheinlichkeitsbasierte Stichprobe, ~7000 Personen
+* Reichhaltige Hintergrunddaten
+
+Eigene Datenerhebung
+====================
+* 6 Wellen von Ende März bis Mitte Dezember
+    * Arbeitsstunden zu Hause und am Arbeitsplatz
+    * Berufsmerkmale: Möglichkeit von zu Hause zu arbeiten; Systemrelevanz der Tätigkeit
+    * Erwartungen
+    * ...
+
+Arbeitsplätze
+====================
+
+![](2021-01/olf-over-time-by-None.svg){width="48%"}
+![](2021-01/unempl-rate-over-time-by-None.svg){width="48%"}
+
+Arbeitsstunden
+==============
+
+* Wir betrachten unbedingte Arbeitsstunden (im Home Office und am Arbeitsplatz)
+
+![](time-variation/hours-home-over-time-by-None-uncond.svg){width="40%"}
+![](time-variation/working-hours-over-time-by-None-uncond.svg){width="40%"}
+
+
+
+Vergleich Dez vs Mär/Apr
+=======================================================
+
+- 52% vs 57% mindestens 1h im HO
+- 73% vs 80% der Stunden im HO (geg. mindestens 1h)
+
+
+Möglichkeit auf Home Office
+=======================================================
+
+![](time-variation/abs-change-hours-home-over-time-by-work_perc_home_cat_only_noness_ger-uncond.svg){width="40%"}
+![](time-variation/abs-change-hours-over-time-by-work_perc_home_cat_only_noness_ger-uncond.svg){width="40%"}
+
+Systemrelevanz
+==========================================================================
+
+![](time-variation/abs-change-hours-home-over-time-by-work_perc_home_cat_only_ess_ger-uncond.svg){width="48%"}
+![](time-variation/abs-change-hours-over-time-by-work_perc_home_cat_only_ess_ger-uncond.svg){width="48%"}
+
+Wer kann von zu Hause arbeiten?
+===============================
+
+*
+
+Entwicklung HO nach Corona?
+===============================================
+
+* Vor Corona: 3,8h im HO
+* Nach Corona erwartet: 8h im HO
+* Nach Corona erwünscht: 9h im HO
+
+
--- task_screencast.py
+++ task_screencast.py
@@ -2,20 +2,78 @@ from pathlib import Path
 
 import pytask
 
+# from config import plain_pandoc
+# from config import revealjs_pandoc
 
 
-from config import plain_pandoc
-from config import revealjs_pandoc
+import pypandoc
 
 
-names = ["labour-market-wellbeing-during-covid-19-netherlands"]
+SRC = Path(__file__).parent
+OUT = SRC.joinpath("out")
+STUD = SRC.parent.joinpath("epp-materials")
+TMP = SRC.joinpath("_tmp")
+
+
+def plain_pandoc(depends_on, produces):
+    if produces.suffix == ".html":
+        pypandoc.convert(
+            str(depends_on),
+            "html",
+            outputfile=str(produces),
+            extra_args=[
+                "--standalone",
+                "--self-contained",
+                "--mathjax",
+                f"--resource-path={depends_on.parent}",
+            ],
+        )
+    elif produces.suffix == ".pdf":
+        pypandoc.convert(
+            str(depends_on),
+            "pdf",
+            outputfile=str(produces),
+            extra_args=["--pdf-engine=xelatex"],
+        )
+    else:
+        raise NotImplementedError(produces.suffix)
+
+
+def revealjs_pandoc(depends_on, produces):
+    pypandoc.convert(
+        str(depends_on[0]),
+        "revealjs",
+        outputfile=str(produces),
+        extra_args=[
+            "--template=" + str(depends_on[1]),
+            "--section-divs",
+            "--mathjax",
+            "-t",
+            "html5",
+            "-V",
+            "theme:night",
+            "-V",
+            "revealjs-url=file://" + str(SRC.absolute()) + "/revealjs",
+            "--no-highlight",
+            "--standalone",
+            "--self-contained",
+            f"--resource-path={depends_on[0].parent}",
+        ],
+    )
+
+
+names = [
+    "labour-market-wellbeing-during-covid-19-netherlands",
+    # "panel_discussion_law"
+]
 
 
 @pytask.mark.parametrize(
     "depends_on, produces",
     [
         (
-            [f"{n}.md", "template-revealjs.html", "config.py"] + list((Path(__file__).parent / "2021-01").glob("*")),
+            [f"{n}.md", "template-revealjs.html"]
+            + list((Path(__file__).parent / "2021-01").glob("*")),
             f"{n}.html",
         )
         for n in names
@@ -23,7 +81,6 @@ names = ["labour-market-wellbeing-during-covid-19-netherlands"]
 )
 def task_convert_revealjs(depends_on, produces):
     return revealjs_pandoc(depends_on, produces)
-
 
 
 # @pytask.mark.parametrize(
